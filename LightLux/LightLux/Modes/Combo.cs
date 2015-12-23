34951a499a4b35feafddbf9745b0d930d489ce47
Damian <damiancz112@gmail.com>
2015-12-23 13:19:50 +0100

    catch

--- LightLux/LightLux/Modes/Combo.cs
+++ LightLux/LightLux/Modes/Combo.cs
@@ -15,53 +15,60 @@ namespace LightLux.Modes
 
         public override void Execute()
         {
-            if (Q.IsReady() && Config.Modes.Combo.UseQ)
+            try
             {
-                var target = TargetSelector.GetTarget(Q.Range, DamageType.Magical, Player.Instance.Position);
-                var qPrediction = Q.GetPrediction(target);
-                if (qPrediction.HitChancePercent <= Program.hitchance && target != null)
+                if (Q.IsReady() && Config.Modes.Combo.UseQ)
                 {
-                    Q.Cast(qPrediction.CastPosition);
-                }
-            }
-
-            if (E.IsReady() && Config.Modes.Combo.UseE)
-            {
-                var target = TargetSelector.GetTarget(E.Range, DamageType.Magical, Player.Instance.Position);
-                if (E.IsInRange(target) && target.IsValidTarget())
-                {
-                    var ePrediction = E.GetPrediction(target);
-                    if (ePrediction.HitChancePercent <= Program.hitchance && target != null)
+                    var target = TargetSelector.GetTarget(Q.Range, DamageType.Magical, Player.Instance.Position);
+                    var qPrediction = Q.GetPrediction(target);
+                    if (qPrediction.HitChancePercent <= Program.hitchance && target != null)
                     {
-                        E.Cast(ePrediction.CastPosition);
+                        Q.Cast(qPrediction.CastPosition);
+                    }
+                }
+
+                if (E.IsReady() && Config.Modes.Combo.UseE)
+                {
+                    var target = TargetSelector.GetTarget(E.Range, DamageType.Magical, Player.Instance.Position);
+                    if (E.IsInRange(target) && target.IsValidTarget())
+                    {
+                        var ePrediction = E.GetPrediction(target);
+                        if (ePrediction.HitChancePercent <= Program.hitchance && target != null)
+                        {
+                            E.Cast(ePrediction.CastPosition);
+                        }
+                    }
+                }
+
+                if (W.IsReady() && Config.Modes.Combo.UseW)
+                {
+                    foreach (var allies in EntityManager.Heroes.Allies)
+                    {
+                        if (allies != null && allies.IsValid)
+                        {
+                            if (allies.IsCharmed || allies.IsAttackingPlayer || allies.IsFeared || allies.IsStunned ||
+                                allies.IsTaunted || allies.IsValid)
+                                W.Cast(allies);
+                        }
+                    }
+                }
+
+                if (R.IsReady() && Config.Modes.Combo.UseR)
+                {
+                    var target = TargetSelector.GetTarget(R.Range, DamageType.Magical, Player.Instance.Position);
+                    if (R.IsInRange(target) && target.IsValidTarget() && target.Health <= Damage.RDamage(target))
+                    {
+                        var rPrediction = R.GetPrediction(target);
+                        if (rPrediction.HitChancePercent <= Program.hitchance && target != null)
+                        {
+                            R.Cast(rPrediction.CastPosition);
+                        }
                     }
                 }
             }
-
-            if (W.IsReady() && Config.Modes.Combo.UseW)
+            catch (Exception stack)
             {
-                foreach (var allies in EntityManager.Heroes.Allies)
-                {
-                    if (allies != null && allies.IsValid)
-                    {
-                        if (allies.IsCharmed || allies.IsAttackingPlayer || allies.IsFeared || allies.IsStunned ||
-                            allies.IsTaunted || allies.IsValid)
-                            W.Cast(allies);
-                    }
-                }
-            }
-
-            if (R.IsReady() && Config.Modes.Combo.UseR)
-            {
-                var target = TargetSelector.GetTarget(R.Range, DamageType.Magical, Player.Instance.Position);
-                if (R.IsInRange(target) && target.IsValidTarget() && target.Health <= Damage.RDamage(target))
-                {
-                    var rPrediction = R.GetPrediction(target);
-                    if (rPrediction.HitChancePercent <= Program.hitchance && target != null)
-                    {
-                        R.Cast(rPrediction.CastPosition);
-                    }
-                }
+                Chat.Print(stack.StackTrace);
             }
         }
     }
